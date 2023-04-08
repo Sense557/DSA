47d3c230342ce460044a779ee73867b5db689e5b
Sense <mehershakti1020@gmail.com>
2023-04-08 09:40:19 +0530

    modified

---
+++ Lecture 5 (Java Operators)/MCQ.java
@@ -0,0 +1,35 @@
+public class MCQ 
+{
+    public static void main(String[] args) 
+    {
+        // //Let say we have 4 variables of type int; p,q,r,s
+        // as postfix operator has more precedence than the arithmetic operator
+        // int p=4, q =3, r=2;
+
+        //     int s = p-++r-++q;
+        //     System.out.println(s);  //-3
+
+        // 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+
+        // //What does the following code fragment print?
+        // as assignment operator has L to R associativity
+        
+        // System.out.println(4+2+"pqr");  //6pqr
+        // System.out.println("pqr"+ 4 + 2);  //pqr42
+
+        // 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+
+        //What does the following code fragment print?
+        //as equality operator has L to R associativity
+        
+        Boolean p = false;
+        Boolean q = false;
+        Boolean r = true;
+
+        System.out.println(p==q==r);  //true
+
+
+
+        
+    }
+}
---
+++ Lecture 5 (Java Operators)/index.java
@@ -0,0 +1,104 @@
+/**
+ *  Java Operators
+ */
+
+
+
+class index
+{
+    public static void main(String[] args) 
+    {
+
+        // 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+        //Java Arithmatic Operators
+        // int p = 20, q = 10;
+
+        // System.out.println(p+q);  //30
+        // System.out.println(p-q);  //10
+        // System.out.println(p*q);  //200
+        // System.out.println(p/q);  //2
+        // System.out.println(p%q);  //0
+
+
+
+        // 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+        //Java Relational Operators
+        // int p = 10, q = 15;
+
+        // System.out.println(p==q);  //false
+        // System.out.println(p!=q);  //true
+        // System.out.println(p<=q);  //true
+        // System.out.println(p>=q);  //false
+        // System.out.println(p<q);  //true
+        // System.out.println(p>q);   //false
+
+
+        // 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+        // Java Logical Operators
+        // int p = 10, q = 20;
+
+
+        //&& Operator
+        // System.out.println((p<q && p<=q));  // true
+        // System.out.println((p<q && p==q));  //false
+        // System.out.println((2==2 && 1==1 && 10==10 && 10>20));  //false
+        
+
+        // || Operator
+        // System.out.println((2==2 || 3==4));   //true
+        // System.out.println((2==10 || 3==4));  //false
+       
+
+        //! Operator
+        // System.out.println(!(p==q));  //true
+        // System.out.println(!(p>q));  //true
+
+        // 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+        // Java Assignment Operator
+        // int p = 10;
+        // int q;
+
+        // // =
+        // q=p;
+        // System.out.println(q); //10
+
+        // // +=
+        // p+=q; //p=p+q, p=10+10=20
+        // System.out.println(p);
+
+        // p-=q;  //p=p-q, p=20-10 = 10
+        // System.out.println(p);
+
+        // p*=q;
+        // System.out.println(p); //100
+
+        // p/=q;
+        // System.out.println(p);  //10
+
+
+
+        // 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+        // Java Unary Operators
+        // int p =5, q = 5;
+
+        // System.out.println(p++);  //5
+        // System.out.println(p);  //6
+
+        // System.out.println(++q);  //6
+        // System.out.println(q);  //6
+
+        // int x = p++;
+        // int y = ++q;
+
+        // System.out.println(x);  //6
+        // System.out.println(y);  //7
+
+        // System.out.println(p);  //7
+        // System.out.println(q);  //7
+
+
+
+        // 🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉🏉
+
+    }
+}
